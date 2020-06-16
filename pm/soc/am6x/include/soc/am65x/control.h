fb6418e212802fabbccec804c99e7db2eb778cd0
Jyri Sarha <jsarha@ti.com>
2020-06-16 11:09:39 +0000

    pm/soc/am6x/include/soc/am65x: Add include guards to control.h and regs.h
    Even empty include files cause MISRA.INCGUARD issue.
    
    Fixes: SYSFW-3440
    
    Signed-off-by: Jyri Sarha <jsarha@ti.com>

--- pm/soc/am6x/include/soc/am65x/control.h
+++ pm/soc/am6x/include/soc/am65x/control.h
@@ -6,3 +6,9 @@
  * Copyright (C) 2017 Texas Instruments Incorporated - http://www.ti.com/
  * ALL RIGHTS RESERVED
  */
+#ifndef PM_AM65X_CONTROL_H
+#define PM_AM65X_CONTROL_H
+
+/* Klockwork MISRA rules requires include guard even for empty include files */
+
+#endif /* PM_AM65X_CONTROL_H */
