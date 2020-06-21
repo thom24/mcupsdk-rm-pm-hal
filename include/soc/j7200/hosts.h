826b2c3d06d9e1db2187174623dd2ee130b28b77
Piyali Goswami <piyali_g@ti.com>
2020-06-21 19:45:43 +0530

    j7200: include: soc: Added J7200 related headers
    Added J7200 related headers as part of rearch
    
    Fixes: SYSFW-3435
    
    Signed-off-by: Piyali Goswami <piyali_g@ti.com>

---
+++ include/soc/j7200/hosts.h
@@ -0,0 +1,52 @@
+/*
+ * System Firmware Source File
+ *
+ * Host IDs for J7200 device
+ *
+ * Data version: 200505_131637
+ *
+ * Copyright (C) 2019-2020 Texas Instruments Incorporated - http://www.ti.com/
+ * ALL RIGHTS RESERVED
+ */
+#ifndef J7200_HOSTS_H
+#define J7200_HOSTS_H
+
+/** DMSC(Secure): Device Management and Security Control */
+#define HOST_ID_DMSC (0U)
+/** MCU_0_R5_0(Non Secure): Cortex R5 context 0 on MCU island */
+#define HOST_ID_MCU_0_R5_0 (3U)
+/** MCU_0_R5_1(Secure): Cortex R5 context 1 on MCU island(Boot) */
+#define HOST_ID_MCU_0_R5_1 (4U)
+/** MCU_0_R5_2(Non Secure): Cortex R5 context 2 on MCU island */
+#define HOST_ID_MCU_0_R5_2 (5U)
+/** MCU_0_R5_3(Secure): Cortex R5 context 3 on MCU island */
+#define HOST_ID_MCU_0_R5_3 (6U)
+/** A72_0(Secure): Cortex A72 context 0 on Main island */
+#define HOST_ID_A72_0 (10U)
+/** A72_1(Secure): Cortex A72 context 1 on Main island */
+#define HOST_ID_A72_1 (11U)
+/** A72_2(Non Secure): Cortex A72 context 2 on Main island */
+#define HOST_ID_A72_2 (12U)
+/** A72_3(Non Secure): Cortex A72 context 3 on Main island */
+#define HOST_ID_A72_3 (13U)
+/** A72_4(Non Secure): Cortex A72 context 4 on Main island */
+#define HOST_ID_A72_4 (14U)
+/** MAIN_0_R5_0(Non Secure): Cortex R5_0 context 0 on Main island */
+#define HOST_ID_MAIN_0_R5_0 (35U)
+/** MAIN_0_R5_1(Secure): Cortex R5_0 context 1 on Main island */
+#define HOST_ID_MAIN_0_R5_1 (36U)
+/** MAIN_0_R5_2(Non Secure): Cortex R5_0 context 2 on Main island */
+#define HOST_ID_MAIN_0_R5_2 (37U)
+/** MAIN_0_R5_3(Secure): Cortex R5_0 context 3 on MCU island */
+#define HOST_ID_MAIN_0_R5_3 (38U)
+
+/**
+ * Host catch all. Used in board configuration resource assignments to define
+ * resource ranges useable by all hosts. Cannot be used
+ */
+#define HOST_ID_ALL (128U)
+
+/** Number of unique hosts on the SoC */
+#define HOST_ID_CNT (14U)
+
+#endif /* J7200_HOSTS_H */
---
+++ include/soc/j7200/soc_devgrps.h
@@ -0,0 +1,36 @@
+/*
+ * System Firmware Source File
+ *
+ * Software defined Device groups (devgrps) for J7200 device.
+ *
+ * Data version: 200505_131637
+ *
+ * Copyright (C) 2019-2020 Texas Instruments Incorporated - http://www.ti.com/
+ * ALL RIGHTS RESERVED
+ */
+#ifndef SOC_J7200_DEVGRPS_H
+#define SOC_J7200_DEVGRPS_H
+
+#include <types/devgrps.h>
+
+/**
+ * Set of MCU domain peripherals to initialize at boot and use during runtime.
+ * Utilize this device group of MCU/Wakeup domain peripherals to achieve a
+ * faster boot sequence
+ */
+#define SOC_DEVGRP_J7200_MCU_WAKEUP DEVGRP_00
+
+/**
+ * Set of Main domain peripherals to initialize at boot and use during
+ * runtime.  Utilize this device group of non-critical peripherals for
+ * advanced use cases.
+ */
+#define SOC_DEVGRP_J7200_MAIN DEVGRP_01
+
+/**
+ * Set of peripherals to initialize for System Firmware internal usage
+ */
+#define SOC_DEVGRP_J7200_DMSC_INTERNAL DEVGRP_DMSC
+
+
+#endif /* SOC_J7200_DEVGRPS_H */
