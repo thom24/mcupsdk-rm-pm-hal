e678d0f413683ad0d47454c7ff51cfc23ef56e87
Piyali Goswami <piyali_g@ti.com>
2020-07-04 17:12:54 +0530

    common: build.mk Only build files when CONFIG_PM or CONFIG_RM is set
    Remove the building of common folder when the CONFIG_PM and CONFIG_RM.
    
    Signed-off-by: Piyali Goswami <piyali_g@ti.com>

--- common/build.mk
+++ common/build.mk
@@ -1,5 +1,13 @@
 # Copyright (C) 2017-2020 Texas Instruments Incorporated - http://www.ti.com/
 # ALL RIGHTS RESERVED
 
-obj-y += boardcfg-user.o
-obj-y += $(TARGET_SOC)/
+INCLUDE_THIS_FILE=n
+ifeq ($(CONFIG_PM),y)
+INCLUDE_THIS_FILE=y
+endif
+ifeq ($(CONFIG_RM),y)
+INCLUDE_THIS_FILE=y
+endif
+
+obj-$(INCLUDE_THIS_FILE) += boardcfg-user.o
+obj-$(INCLUDE_THIS_FILE) += $(TARGET_SOC)/
