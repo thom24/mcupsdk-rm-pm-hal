ba1f51185d6b8072969a63acbda1f76a72756026
Jyri Sarha <jsarha@ti.com>
2020-08-06 13:41:46 +0000

    common/am6x/mmr_lock.c: Fix all MISRA-C issues
    The most of the changes are quite trivial and easy to understand. However,
    the MISRA.INCR_DECR.SIDEEFF.2012 issues fixed by changing -- and ++ operator
    use to simple = +1 and -1 is fixing an obvious false positive. But since
    getting a Deviation Report trough is so tedious job and there is only couple
    of false positives of this kind, it is easier just to get rid of ++ and --
    usage all together. Anyway, when following MISRA-C rules, there really is
    no benefit in using increment or decrement operators anywhere.
    
    Fixes: SYSFW-1510
    
    Signed-off-by: Jyri Sarha <jsarha@ti.com>

--- common/am6x/mmr_lock.c
+++ common/am6x/mmr_lock.c
@@ -16,14 +16,14 @@
 #define WKUP_CTRL_BASE  0x43000000
 #define MCU_CTRL_BASE   0x40f00000
 
-#define CTRL_MMR0_PARTITION_SIZE                        0x4000
+#define CTRL_MMR0_PARTITION_SIZE                        0x4000U
 
-#define CTRLMMR_LOCK_KICK0                              0x01008
+#define CTRLMMR_LOCK_KICK0                              0x01008U
 #define CTRLMMR_LOCK_KICK0_UNLOCK_VAL                   0x68ef3490
 #define CTRLMMR_LOCK_KICK0_LOCK_VAL                     0x0
 #define CTRLMMR_LOCK_KICK0_UNLOCKED_MASK                BIT(0)
 #define CTRLMMR_LOCK_KICK0_UNLOCKED_SHIFT               0
-#define CTRLMMR_LOCK_KICK1                              0x0100c
+#define CTRLMMR_LOCK_KICK1                              0x0100cU
 #define CTRLMMR_LOCK_KICK1_UNLOCK_VAL                   0xd172bc5a
 #define CTRLMMR_LOCK_KICK1_LOCK_VAL                     0x0
 
@@ -32,7 +32,7 @@ static volatile s32 mmr_lock_count = 0;
 static void mmr_unlock(u32 base, u32 partition)
 {
 	/* Translate the base address */
-	u32 part_base = base + partition * CTRL_MMR0_PARTITION_SIZE;
+	u32 part_base = base + (partition * CTRL_MMR0_PARTITION_SIZE);
 
 	/* Unlock the requested partition if locked using two-step sequence */
 	writel(CTRLMMR_LOCK_KICK0_UNLOCK_VAL, part_base + CTRLMMR_LOCK_KICK0);
@@ -42,18 +42,18 @@ static void mmr_unlock(u32 base, u32 partition)
 static void mmr_lock(u32 base, u32 partition)
 {
 	/* Translate the base address */
-	u32 part_base = base + partition * CTRL_MMR0_PARTITION_SIZE;
+	u32 part_base = base + (partition * CTRL_MMR0_PARTITION_SIZE);
 
 	/* Unlock the requested partition if locked using two-step sequence */
 	writel(CTRLMMR_LOCK_KICK0_LOCK_VAL, part_base + CTRLMMR_LOCK_KICK0);
 	writel(CTRLMMR_LOCK_KICK1_LOCK_VAL, part_base + CTRLMMR_LOCK_KICK1);
 }
 
-void mmr_lock_all()
+void mmr_lock_all(void)
 {
 	u32 key = osal_hwip_disable();
 
-	mmr_lock_count--;
+	mmr_lock_count = mmr_lock_count - 1;
 	if (mmr_lock_count == 0) {
 		/* lock CLKSEL MMR partitions */
 		mmr_lock(WKUP_CTRL_BASE, 2);
@@ -65,7 +65,7 @@ void mmr_lock_all()
 	osal_hwip_restore(key);
 }
 
-void mmr_unlock_all()
+void mmr_unlock_all(void)
 {
 	u32 key = osal_hwip_disable();
 
@@ -77,6 +77,6 @@ void mmr_unlock_all()
 		/* Unlock WKUP_MCU_WARM_RST_CTRL MMR partitions*/
 		mmr_unlock(WKUP_CTRL_BASE, 6);
 	}
-	mmr_lock_count++;
+	mmr_lock_count = mmr_lock_count + 1;
 	osal_hwip_restore(key);
 }
