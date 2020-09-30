54fd4496840e9fdb0d7b18ff6d4729f730f9e579
Jyri Sarha <jsarha@ti.com>
2020-09-30 13:31:43 +0000

    pm/core/sleep.c: Fix all MISRA.CT.UNIQUE.ID issues
    Fixes: SYSFW-1510
    
    Signed-off-by: Jyri Sarha <jsarha@ti.com>

--- pm/core/sleep.c
+++ pm/core/sleep.c
@@ -45,7 +45,7 @@ static const struct sleep_mode *sleep_modes __attribute__((unused));
 static u8 *sleep_modes_block __attribute__((unused));
 static size_t sleep_mode_count __attribute__((unused));
 
-void sleep_mode_update(u8 sleep_mode __attribute__((unused)), sbool allow __attribute__((unused)))
+void sleep_mode_update(u8 sleepmode __attribute__((unused)), sbool allow __attribute__((unused)))
 {
 }
 
