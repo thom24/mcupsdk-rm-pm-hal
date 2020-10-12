d0410943d69d02f5073a95b682af0bc61f9c979b
Dave Gerlach <d-gerlach@ti.com>
2020-10-12 21:56:05 +0000

    pm: soc: Sync with latest data for CLK_DATA_FLAG_NO_HW_REINIT flag
    For j7200 we must set the CLK_DATA_FLAG_NO_HW_REINIT for Main PLL 14 and
    the associated hsdiv0 so that the PULSAR PLL is not reconfigured if it
    is not locked.
    
    Fixes: SYSFW-4194 #integrate-and-build
    
    Signed-off-by: Dave Gerlach <d-gerlach@ti.com>

--- pm/soc/j7200/clocks.c
+++ pm/soc/j7200/clocks.c
@@ -5826,7 +5826,7 @@ const struct clk_data soc_clock_data[] = {
 		.drv		= &clk_drv_pll_16fft,
 		.freq_idx	= J7200_FREQ_VALUE_PLLFRACF_SSMOD_16FFT_MAIN_14,
 		.data		= &clk_data_pllfracf_ssmod_16fft_main_14.data_pll.data,
-		.flags		= 0,
+		.flags		= CLK_DATA_FLAG_NO_HW_REINIT,
 	},
 	[CLK_J7200_PLLFRACF_SSMOD_16FFT_MAIN_2_FOUTVCOP_CLK] =		{
 		.parent		=					{
@@ -6595,7 +6595,7 @@ const struct clk_data soc_clock_data[] = {
 			1,
 		},
 		.drv	= &clk_drv_div_pll_16fft_hsdiv.drv,
-		.flags	= CLK_DATA_FLAG_MODIFY_PARENT_FREQ,
+		.flags	= CLK_DATA_FLAG_MODIFY_PARENT_FREQ | CLK_DATA_FLAG_NO_HW_REINIT,
 		.type	= CLK_TYPE_DIV,
 		.data	= &clk_data_hsdiv1_16fft_main_14_hsdiv0.data_div.data,
 	},
