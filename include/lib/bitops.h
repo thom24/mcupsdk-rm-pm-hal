982a6334ca0159e9a0b527ccf652b8a6b3000982
Jyri Sarha <jsarha@ti.com>
2020-12-12 22:51:21 +0000

    include/lib/bitops.h: Fix MISRA.ETYPE.ASSIGN.2012 etc. issues
    For the time being this is an idle fix since this header is not actually
    used in sysfw build. The one in system-firmware/common/include/lib/ is
    used instead.
    
    Fixes: SYSFW-1510
    
    Signed-off-by: Jyri Sarha <jsarha@ti.com>

--- include/lib/bitops.h
+++ include/lib/bitops.h
@@ -40,40 +40,40 @@
 #include <types/short_types.h>
 
 /** BIT macro for easily calculating a bit at offset (n) */
-#define BIT(n)  (1UL << (n))
+#define BIT(n)  (((u32) 1U) << (n))
 
-#define ORDER_1B        0
-#define ORDER_2B        1
-#define ORDER_4B        2
-#define ORDER_8B        3
-#define ORDER_16B       4
-#define ORDER_32B       5
-#define ORDER_64B       6
-#define ORDER_128B      7
-#define ORDER_256B      8
-#define ORDER_512B      9
-#define ORDER_1KB       10
-#define ORDER_2KB       11
-#define ORDER_4KB       12
-#define ORDER_8KB       13
-#define ORDER_16KB      14
-#define ORDER_32KB      15
-#define ORDER_64KB      16
-#define ORDER_128KB     17
-#define ORDER_256KB     18
-#define ORDER_512KB     19
-#define ORDER_1MB       20
-#define ORDER_2MB       21
-#define ORDER_4MB       22
-#define ORDER_8MB       23
-#define ORDER_16MB      24
-#define ORDER_32MB      25
-#define ORDER_64MB      26
-#define ORDER_128MB     27
-#define ORDER_256MB     28
-#define ORDER_512MB     29
-#define ORDER_1GB       30
-#define ORDER_2GB       31
+#define ORDER_1B        0U
+#define ORDER_2B        1U
+#define ORDER_4B        2U
+#define ORDER_8B        3U
+#define ORDER_16B       4U
+#define ORDER_32B       5U
+#define ORDER_64B       6U
+#define ORDER_128B      7U
+#define ORDER_256B      8U
+#define ORDER_512B      9U
+#define ORDER_1KB       10U
+#define ORDER_2KB       11U
+#define ORDER_4KB       12U
+#define ORDER_8KB       13U
+#define ORDER_16KB      14U
+#define ORDER_32KB      15U
+#define ORDER_64KB      16U
+#define ORDER_128KB     17U
+#define ORDER_256KB     18U
+#define ORDER_512KB     19U
+#define ORDER_1MB       20U
+#define ORDER_2MB       21U
+#define ORDER_4MB       22U
+#define ORDER_8MB       23U
+#define ORDER_16MB      24U
+#define ORDER_32MB      25U
+#define ORDER_64MB      26U
+#define ORDER_128MB     27U
+#define ORDER_256MB     28U
+#define ORDER_512MB     29U
+#define ORDER_1GB       30U
+#define ORDER_2GB       31U
 
 static inline u32 get_field(u32 val, u32 end, u32 start);
 static inline u32 set_field(u32 tgt, u32 end, u32 start, u32 val);
