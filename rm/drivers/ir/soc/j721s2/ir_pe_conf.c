29bc1c7efa1cccca5f32662f2fda938c4ae1e1dd
Jonathan Bergsagel <jbergsagel@ti.com>
2021-02-06 00:45:46 +0000

    j721s2: Add SoC data for j721s2 device
    This introduces j721s2 data into rm_pm_hal as a buildable SoC
    configuration
    
    Fixes: SYSFW-4940
    
    Signed-off-by: Jonathan Bergsagel <jbergsagel@ti.com>
    Signed-off-by: Sheng Zhao shengzhao@ti.com
    Signed-off-by: Stephen Molfetta <sjmolfetta@ti.com>

---
+++ rm/drivers/ir/soc/j721s2/ir_pe_conf.c
@@ -0,0 +1,455 @@
+/*
+ * System Firmware Source File
+ *
+ * Static IR IRQ maps for Processing Entities for J721S2 device
+ *
+ * Data version: 210712_144506
+ *
+ * Copyright (C) 2021, Texas Instruments Incorporated
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * *  Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ * *  Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *
+ * *  Neither the name of Texas Instruments Incorporated nor the names of
+ * its contributors may be used to endorse or promote products derived
+ * from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <devices.h>
+
+#include "../../include/ir_init.h"
+#include "ir_pe_conf.h"
+
+const struct ir_init ir_soc_pe_init_list[IR_SOC_PE_INIT_NUM] = {
+	/*
+	 * For: A72_0 notify: COMPUTE_CLUSTER0_CLEC/soc_events_in_64,
+	 * COMPUTE_CLUSTER0_GIC500SS/spi_64
+	 */
+	[0] =  {
+		.id	= J721S2_DEV_NAVSS0_INTR_0,
+		.inp	= 0x00U,
+		.outp	= 0x0000U,
+	},
+	/*
+	 * For: A72_0 response: COMPUTE_CLUSTER0_CLEC/soc_events_in_65,
+	 * COMPUTE_CLUSTER0_GIC500SS/spi_65
+	 */
+	[1] =  {
+		.id	= J721S2_DEV_NAVSS0_INTR_0,
+		.inp	= 0x01U,
+		.outp	= 0x0001U,
+	},
+	/*
+	 * For: A72_1 notify: COMPUTE_CLUSTER0_CLEC/soc_events_in_66,
+	 * COMPUTE_CLUSTER0_GIC500SS/spi_66
+	 */
+	[2] =  {
+		.id	= J721S2_DEV_NAVSS0_INTR_0,
+		.inp	= 0x02U,
+		.outp	= 0x0002U,
+	},
+	/*
+	 * For: A72_1 response: COMPUTE_CLUSTER0_CLEC/soc_events_in_67,
+	 * COMPUTE_CLUSTER0_GIC500SS/spi_67
+	 */
+	[3] =  {
+		.id	= J721S2_DEV_NAVSS0_INTR_0,
+		.inp	= 0x03U,
+		.outp	= 0x0003U,
+	},
+	/*
+	 * For: A72_2 notify: COMPUTE_CLUSTER0_CLEC/soc_events_in_68,
+	 * COMPUTE_CLUSTER0_GIC500SS/spi_68
+	 */
+	[4] =  {
+		.id	= J721S2_DEV_NAVSS0_INTR_0,
+		.inp	= 0x04U,
+		.outp	= 0x0004U,
+	},
+	/*
+	 * For: A72_2 response: COMPUTE_CLUSTER0_CLEC/soc_events_in_69,
+	 * COMPUTE_CLUSTER0_GIC500SS/spi_69
+	 */
+	[5] =  {
+		.id	= J721S2_DEV_NAVSS0_INTR_0,
+		.inp	= 0x05U,
+		.outp	= 0x0005U,
+	},
+	/*
+	 * For: A72_3 notify: COMPUTE_CLUSTER0_CLEC/soc_events_in_70,
+	 * COMPUTE_CLUSTER0_GIC500SS/spi_70
+	 */
+	[6] =  {
+		.id	= J721S2_DEV_NAVSS0_INTR_0,
+		.inp	= 0x06U,
+		.outp	= 0x0006U,
+	},
+	/*
+	 * For: A72_3 response: COMPUTE_CLUSTER0_CLEC/soc_events_in_71,
+	 * COMPUTE_CLUSTER0_GIC500SS/spi_71
+	 */
+	[7] =  {
+		.id	= J721S2_DEV_NAVSS0_INTR_0,
+		.inp	= 0x07U,
+		.outp	= 0x0007U,
+	},
+	/*
+	 * For: A72_4 notify: COMPUTE_CLUSTER0_CLEC/soc_events_in_72,
+	 * COMPUTE_CLUSTER0_GIC500SS/spi_72
+	 */
+	[8] =  {
+		.id	= J721S2_DEV_NAVSS0_INTR_0,
+		.inp	= 0x08U,
+		.outp	= 0x0008U,
+	},
+	/*
+	 * For: A72_4 response: COMPUTE_CLUSTER0_CLEC/soc_events_in_73,
+	 * COMPUTE_CLUSTER0_GIC500SS/spi_73
+	 */
+	[9] =  {
+		.id	= J721S2_DEV_NAVSS0_INTR_0,
+		.inp	= 0x09U,
+		.outp	= 0x0009U,
+	},
+	/*
+	 * For: C7X_0_0 notify: COMPUTE_CLUSTER0_CLEC/soc_events_in_728,
+	 * COMPUTE_CLUSTER0_GIC500SS/spi_728
+	 */
+	[10] = {
+		.id	= J721S2_DEV_NAVSS0_INTR_0,
+		.inp	= 0x0AU,
+		.outp	= 0x00B8U,
+	},
+	/*
+	 * For: C7X_0_0 response: COMPUTE_CLUSTER0_CLEC/soc_events_in_729,
+	 * COMPUTE_CLUSTER0_GIC500SS/spi_729
+	 */
+	[11] = {
+		.id	= J721S2_DEV_NAVSS0_INTR_0,
+		.inp	= 0x0BU,
+		.outp	= 0x00B9U,
+	},
+	/*
+	 * For: C7X_0_1 notify: COMPUTE_CLUSTER0_CLEC/soc_events_in_730,
+	 * COMPUTE_CLUSTER0_GIC500SS/spi_730
+	 */
+	[12] = {
+		.id	= J721S2_DEV_NAVSS0_INTR_0,
+		.inp	= 0x0CU,
+		.outp	= 0x00BAU,
+	},
+	/*
+	 * For: C7X_0_1 response: COMPUTE_CLUSTER0_CLEC/soc_events_in_731,
+	 * COMPUTE_CLUSTER0_GIC500SS/spi_731
+	 */
+	[13] = {
+		.id	= J721S2_DEV_NAVSS0_INTR_0,
+		.inp	= 0x0DU,
+		.outp	= 0x00BBU,
+	},
+	/*
+	 * For: C7X_1_0 notify: COMPUTE_CLUSTER0_CLEC/soc_events_in_732,
+	 * COMPUTE_CLUSTER0_GIC500SS/spi_732
+	 */
+	[14] = {
+		.id	= J721S2_DEV_NAVSS0_INTR_0,
+		.inp	= 0x0EU,
+		.outp	= 0x00BCU,
+	},
+	/*
+	 * For: C7X_1_0 response: COMPUTE_CLUSTER0_CLEC/soc_events_in_733,
+	 * COMPUTE_CLUSTER0_GIC500SS/spi_733
+	 */
+	[15] = {
+		.id	= J721S2_DEV_NAVSS0_INTR_0,
+		.inp	= 0x0FU,
+		.outp	= 0x00BDU,
+	},
+	/*
+	 * For: C7X_1_1 notify: COMPUTE_CLUSTER0_CLEC/soc_events_in_734,
+	 * COMPUTE_CLUSTER0_GIC500SS/spi_734
+	 */
+	[16] = {
+		.id	= J721S2_DEV_NAVSS0_INTR_0,
+		.inp	= 0x10U,
+		.outp	= 0x00BEU,
+	},
+	/*
+	 * For: C7X_1_1 response: COMPUTE_CLUSTER0_CLEC/soc_events_in_735,
+	 * COMPUTE_CLUSTER0_GIC500SS/spi_735
+	 */
+	[17] = {
+		.id	= J721S2_DEV_NAVSS0_INTR_0,
+		.inp	= 0x11U,
+		.outp	= 0x00BFU,
+	},
+	/*
+	 * For: MAIN_0_R5_0 notify: R5FSS0_CORE0/intr_224
+	 */
+	[18] = {
+		.id	= J721S2_DEV_NAVSS0_INTR_0,
+		.inp	= 0x12U,
+		.outp	= 0x00C0U,
+	},
+	/*
+	 * For: MAIN_0_R5_0 response: R5FSS0_CORE0/intr_225
+	 */
+	[19] = {
+		.id	= J721S2_DEV_NAVSS0_INTR_0,
+		.inp	= 0x13U,
+		.outp	= 0x00C1U,
+	},
+	/*
+	 * For: MAIN_0_R5_1 notify: R5FSS0_CORE0/intr_226
+	 */
+	[20] = {
+		.id	= J721S2_DEV_NAVSS0_INTR_0,
+		.inp	= 0x14U,
+		.outp	= 0x00C2U,
+	},
+	/*
+	 * For: MAIN_0_R5_1 response: R5FSS0_CORE0/intr_227
+	 */
+	[21] = {
+		.id	= J721S2_DEV_NAVSS0_INTR_0,
+		.inp	= 0x15U,
+		.outp	= 0x00C3U,
+	},
+	/*
+	 * For: MAIN_0_R5_2 notify: R5FSS0_CORE1/intr_224
+	 */
+	[22] = {
+		.id	= J721S2_DEV_NAVSS0_INTR_0,
+		.inp	= 0x16U,
+		.outp	= 0x00E0U,
+	},
+	/*
+	 * For: MAIN_0_R5_2 response: R5FSS0_CORE1/intr_225
+	 */
+	[23] = {
+		.id	= J721S2_DEV_NAVSS0_INTR_0,
+		.inp	= 0x17U,
+		.outp	= 0x00E1U,
+	},
+	/*
+	 * For: MAIN_0_R5_3 notify: R5FSS0_CORE1/intr_226
+	 */
+	[24] = {
+		.id	= J721S2_DEV_NAVSS0_INTR_0,
+		.inp	= 0x18U,
+		.outp	= 0x00E2U,
+	},
+	/*
+	 * For: MAIN_0_R5_3 response: R5FSS0_CORE1/intr_227
+	 */
+	[25] = {
+		.id	= J721S2_DEV_NAVSS0_INTR_0,
+		.inp	= 0x19U,
+		.outp	= 0x00E3U,
+	},
+	/*
+	 * For: MAIN_1_R5_0 notify: R5FSS1_CORE0/intr_224
+	 */
+	[26] = {
+		.id	= J721S2_DEV_NAVSS0_INTR_0,
+		.inp	= 0x1AU,
+		.outp	= 0x0100U,
+	},
+	/*
+	 * For: MAIN_1_R5_0 response: R5FSS1_CORE0/intr_225
+	 */
+	[27] = {
+		.id	= J721S2_DEV_NAVSS0_INTR_0,
+		.inp	= 0x1BU,
+		.outp	= 0x0101U,
+	},
+	/*
+	 * For: MAIN_1_R5_1 notify: R5FSS1_CORE0/intr_226
+	 */
+	[28] = {
+		.id	= J721S2_DEV_NAVSS0_INTR_0,
+		.inp	= 0x1CU,
+		.outp	= 0x0102U,
+	},
+	/*
+	 * For: MAIN_1_R5_1 response: R5FSS1_CORE0/intr_227
+	 */
+	[29] = {
+		.id	= J721S2_DEV_NAVSS0_INTR_0,
+		.inp	= 0x1DU,
+		.outp	= 0x0103U,
+	},
+	/*
+	 * For: MAIN_1_R5_2 notify: R5FSS1_CORE1/intr_224
+	 */
+	[30] = {
+		.id	= J721S2_DEV_NAVSS0_INTR_0,
+		.inp	= 0x1EU,
+		.outp	= 0x0120U,
+	},
+	/*
+	 * For: MAIN_1_R5_2 response: R5FSS1_CORE1/intr_225
+	 */
+	[31] = {
+		.id	= J721S2_DEV_NAVSS0_INTR_0,
+		.inp	= 0x1FU,
+		.outp	= 0x0121U,
+	},
+	/*
+	 * For: MAIN_1_R5_3 notify: R5FSS1_CORE1/intr_226
+	 */
+	[32] = {
+		.id	= J721S2_DEV_NAVSS0_INTR_0,
+		.inp	= 0x20U,
+		.outp	= 0x0122U,
+	},
+	/*
+	 * For: MAIN_1_R5_3 response: R5FSS1_CORE1/intr_227
+	 */
+	[33] = {
+		.id	= J721S2_DEV_NAVSS0_INTR_0,
+		.inp	= 0x21U,
+		.outp	= 0x0123U,
+	},
+	/*
+	 * For: MCU_0_R5_0 notify: MCU_R5FSS0_CORE0/intr_64
+	 */
+	[34] = {
+		.id	= J721S2_DEV_MCU_NAVSS0_INTR_ROUTER_0,
+		.inp	= 0x00U,
+		.outp	= 0x0000U,
+	},
+	/*
+	 * For: MCU_0_R5_0 response: MCU_R5FSS0_CORE0/intr_65
+	 */
+	[35] = {
+		.id	= J721S2_DEV_MCU_NAVSS0_INTR_ROUTER_0,
+		.inp	= 0x01U,
+		.outp	= 0x0001U,
+	},
+	/*
+	 * For: MCU_0_R5_1 notify: MCU_R5FSS0_CORE0/intr_66
+	 */
+	[36] = {
+		.id	= J721S2_DEV_MCU_NAVSS0_INTR_ROUTER_0,
+		.inp	= 0x02U,
+		.outp	= 0x0002U,
+	},
+	/*
+	 * For: MCU_0_R5_1 response: MCU_R5FSS0_CORE0/intr_67
+	 */
+	[37] = {
+		.id	= J721S2_DEV_MCU_NAVSS0_INTR_ROUTER_0,
+		.inp	= 0x03U,
+		.outp	= 0x0003U,
+	},
+	/*
+	 * For: MCU_0_R5_2 notify: MCU_R5FSS0_CORE1/intr_64
+	 */
+	[38] = {
+		.id	= J721S2_DEV_MCU_NAVSS0_INTR_ROUTER_0,
+		.inp	= 0x04U,
+		.outp	= 0x0020U,
+	},
+	/*
+	 * For: MCU_0_R5_2 response: MCU_R5FSS0_CORE1/intr_65
+	 */
+	[39] = {
+		.id	= J721S2_DEV_MCU_NAVSS0_INTR_ROUTER_0,
+		.inp	= 0x05U,
+		.outp	= 0x0021U,
+	},
+	/*
+	 * For: MCU_0_R5_3 notify: MCU_R5FSS0_CORE1/intr_66
+	 */
+	[40] = {
+		.id	= J721S2_DEV_MCU_NAVSS0_INTR_ROUTER_0,
+		.inp	= 0x06U,
+		.outp	= 0x0022U,
+	},
+	/*
+	 * For: MCU_0_R5_3 response: MCU_R5FSS0_CORE1/intr_67
+	 */
+	[41] = {
+		.id	= J721S2_DEV_MCU_NAVSS0_INTR_ROUTER_0,
+		.inp	= 0x07U,
+		.outp	= 0x0023U,
+	},
+	/*
+	 * For: DM err_event: MCU_R5FSS0_CORE0/intr_69
+	 */
+	[42] = {
+		.id	= J721S2_DEV_MCU_NAVSS0_INTR_ROUTER_0,
+		.inp	= 0x10U,
+		.outp	= 0x0005U,
+	},
+	/*
+	 * For: DM nonsec_high_priority_rx: MCU_R5FSS0_CORE0/intr_70
+	 */
+	[43] = {
+		.id	= J721S2_DEV_MCU_NAVSS0_INTR_ROUTER_0,
+		.inp	= 0x11U,
+		.outp	= 0x0006U,
+	},
+	/*
+	 * For: DM nonsec_high_priority_rx: MCU_R5FSS0_CORE0/intr_71
+	 */
+	[44] = {
+		.id	= J721S2_DEV_MCU_NAVSS0_INTR_ROUTER_0,
+		.inp	= 0x12U,
+		.outp	= 0x0007U,
+	},
+	/*
+	 * For: DM nonsec_low_priority_rx: MCU_R5FSS0_CORE0/intr_72
+	 */
+	[45] = {
+		.id	= J721S2_DEV_MCU_NAVSS0_INTR_ROUTER_0,
+		.inp	= 0x13U,
+		.outp	= 0x0008U,
+	},
+	/*
+	 * For: DM nonsec_low_priority_rx: MCU_R5FSS0_CORE0/intr_73
+	 */
+	[46] = {
+		.id	= J721S2_DEV_MCU_NAVSS0_INTR_ROUTER_0,
+		.inp	= 0x14U,
+		.outp	= 0x0009U,
+	},
+	/*
+	 * For: DM nonsec_notify_resp_rx: MCU_R5FSS0_CORE0/intr_74
+	 */
+	[47] = {
+		.id	= J721S2_DEV_MCU_NAVSS0_INTR_ROUTER_0,
+		.inp	= 0x15U,
+		.outp	= 0x000AU,
+	},
+	/*
+	 * For: DM nonsec_notify_resp_rx: MCU_R5FSS0_CORE0/intr_75
+	 */
+	[48] = {
+		.id	= J721S2_DEV_MCU_NAVSS0_INTR_ROUTER_0,
+		.inp	= 0x16U,
+		.outp	= 0x000BU,
+	},
+};
---
+++ rm/drivers/udmap/soc/j721s2/udmap_cfg.h
@@ -0,0 +1,107 @@
+/*
+ * UDMAP
+ *
+ * UDMAP SoC-specific configuration definitions
+ *
+ * Data version: 210712_144506
+ *
+ * Copyright (C) 2021, Texas Instruments Incorporated
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * *  Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ * *  Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *
+ * *  Neither the name of Texas Instruments Incorporated nor the names of
+ * its contributors may be used to endorse or promote products derived
+ * from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+#ifndef UDMAP_CFG_H
+#define UDMAP_CFG_H
+
+/**
+ * NAVSS0_BCDMA_0 total number of UDMAP transmit channels
+ */
+#define UDMAP_NAVSS0_BCDMA_0_TX_CHAN_MAX_INDEX (16U)
+/**
+ * NAVSS0_BCDMA_0 total number of UDMAP receive channels
+ */
+#define UDMAP_NAVSS0_BCDMA_0_RX_CHAN_MAX_INDEX (32U)
+/**
+ * NAVSS0_UDMAP_0 total number of UDMAP transmit channels
+ */
+#define UDMAP_NAVSS0_UDMAP_0_TX_CHAN_MAX_INDEX (341U)
+/**
+ * NAVSS0_UDMAP_0 total number of UDMAP receive channels
+ */
+#define UDMAP_NAVSS0_UDMAP_0_RX_CHAN_MAX_INDEX (82U)
+/**
+ * NAVSS0_UDMAP_0 total number of UDMAP receive flows
+ */
+#define UDMAP_NAVSS0_UDMAP_0_RX_FLOW_MAX_INDEX (224U)
+/**
+ * MCU_NAVSS0_UDMAP_0 total number of UDMAP transmit channels
+ */
+#define UDMAP_MCU_NAVSS0_UDMAP_0_TX_CHAN_MAX_INDEX (48U)
+/**
+ * MCU_NAVSS0_UDMAP_0 total number of UDMAP receive channels
+ */
+#define UDMAP_MCU_NAVSS0_UDMAP_0_RX_CHAN_MAX_INDEX (48U)
+/**
+ * MCU_NAVSS0_UDMAP_0 total number of UDMAP receive flows
+ */
+#define UDMAP_MCU_NAVSS0_UDMAP_0_RX_FLOW_MAX_INDEX (96U)
+/**
+ * MCU_SA3_SS0_PKTDMA_0 total number of UDMAP transmit channels
+ */
+#define UDMAP_MCU_SA3_SS0_PKTDMA_0_TX_CHAN_MAX_INDEX (1U)
+/**
+ * MCU_SA3_SS0_PKTDMA_0 total number of UDMAP receive channels
+ */
+#define UDMAP_MCU_SA3_SS0_PKTDMA_0_RX_CHAN_MAX_INDEX (2U)
+/**
+ * MCU_SA3_SS0_PKTDMA_0 total number of UDMAP receive flows
+ */
+#define UDMAP_MCU_SA3_SS0_PKTDMA_0_RX_FLOW_MAX_INDEX (16U)
+/**
+ * UDMAP transmit channel OES index offset
+ */
+#define UDMAP_TCHAN_OES_EVT_INDEX_OFFSET (0x0U)
+/**
+ * UDMAP transmit channel error OES index offset
+ */
+#define UDMAP_TCHAN_EOES_EVT_INDEX_OFFSET (0x200U)
+/**
+ * UDMAP receive channel OES index offset
+ */
+#define UDMAP_RCHAN_OES_EVT_INDEX_OFFSET (0x400U)
+/**
+ * UDMAP receive channel error OES index offset
+ */
+#define UDMAP_RCHAN_EOES_EVT_INDEX_OFFSET (0x480U)
+/**
+ * UDMAP invalid flow event index.  Must be greater than the max of all SoC
+ * UDMAPs channel OES indexes to avoid event indexing conflicts.
+ */
+#define UDMAP_GCFG_INVALID_FLOW_EVT_INDEX (0x500U)
+
+#endif /* UDMAP_CFG_H */
