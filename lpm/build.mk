ce1d58a09d91f13824bd24781cdbe80b75af92eb
Bin Liu <b-liu@ti.com>
2021-05-05 21:26:58 -0500

    lpm: am62x: Add SoC Specific headers for am62x data
    Introduce AM62x specific data under an soc specific path to allow future
    SoCs to share the same code path.
    
    Sync with latest autogen to get new lpm_data files. Currently these
    files contain autogenerated wakeup source data.
    
    Fixes: SYSFW-4623 #integrate-and-build
    
    Signed-off-by: Dave Gerlach <d-gerlach@ti.com>
    Signed-off-by: Bin Liu <b-liu@ti.com>

--- lpm/build.mk
+++ lpm/build.mk
@@ -1,3 +1,7 @@
-TARGET_SOC = $(shell echo $(CONFIG_SOC_STRING))
+cppflags-$(CONFIG_LPM_DM_STUB) += -I$(srctree)/lpm/include
+cppflags-$(CONFIG_LPM_DM_STUB) += -I$(srctree)/pm/include
+cppflags-$(CONFIG_LPM_DM_STUB) += -I$(srctree)/lpm/include/soc/$(TARGET_SOC)
+
+obj-y += soc/
 
 obj-y += main.o
