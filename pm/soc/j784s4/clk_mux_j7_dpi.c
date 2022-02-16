8eed09c40d7b35d98aaa23b996ff4540e02f11c9
Sheng Zhao <shengzhao@ti.com>
2022-02-16 19:09:02 +0000

    Add support for j784s4
    The RM and PM data are generated by Autogen.
    
    Additional files not generated by Autogen are also added, referencing
    files from j721s2.
    
    Signed-off-by: Aditya Wadhwa <a-wadhwa@ti.com>
    Signed-off-by: Sheng Zhao <shengzhao@ti.com>

---
+++ pm/soc/j784s4/clk_mux_j7_dpi.c
@@ -0,0 +1,442 @@
+/*
+ * DMSC firmware
+ *
+ * Copyright (C) 2019-2022, Texas Instruments Incorporated
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * *  Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ * *  Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *
+ * *  Neither the name of Texas Instruments Incorporated nor the names of
+ * its contributors may be used to endorse or promote products derived
+ * from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <soc/j784s4/clk_mux_j7_dpi.h>
+#include <lib/container_of.h>
+#include <ilog.h>
+#include <lib/ioremap.h>
+#include <clk_mux.h>
+#include <compiler.h>
+#include <lib/trace.h>
+
+#define J7_MAIN_CTRL_MMR        0x00100000UL
+
+/*
+ * 1'b0 - MAIN_PLL16_HSDIV0_CLKOUT
+ * 1'b1 - MAIN_PLL17_HSDIV0_CLKOUT
+ */
+#define J7_DSS_DISPC0_CLKSEL2   0x8328UL
+
+/*
+ * 3'b000 - MAIN_PLL16_HSDIV1_CLKOUT
+ * 3'b001 - MAIN_PLL17_HSDIV1_CLKOUT
+ * 3'b010 - MAIN_PLL17_HSDIV1_CLKOUT
+ * 3'b011 - MAIN_PLL19_HSDIV0_CLKOUT_EXTPCLKIN
+ * 3'b100 - MAIN_PLL19_HSDIV0_CLKOUT_EXTPCLKIN
+ * 3'b101 - MAIN_PLL19_HSDIV0_CLKOUT_EXTPCLKIN and dpi0_pclk = dpi1_pclk
+ * 3'b110 - MAIN_PLL19_HSDIV0_CLKOUT_EXTPCLKIN and dpi2_pclk = MAIN_PLL19_HSDIV0_CLKOUT_EXTPCLKIN
+ * 3'b111 - MAIN_PLL19_HSDIV0_CLKOUT_EXTPCLKIN and dpi0_pclk = dpi1_pclk
+ *                                             and dpi2_pclk = MAIN_PLL19_HSDIV0_CLKOUT_EXTPCLKIN
+ */
+#define J7_DSS_DISPC0_CLKSEL3   0x832cUL
+
+#ifdef CONFIG_TRACE
+#define TRACE_ONLY
+#else
+#define TRACE_ONLY __attribute__((unused))
+#endif
+
+
+static const struct clk_parent *clk_mux_j7_dpi_0_pclk_get_parent(struct clk *clk)
+{
+	const struct clk_data *clk_data = clk_get_data(clk);
+	const struct clk_data_mux *mux;
+	u32 v;
+
+	mux = container_of(clk_data->data, const struct clk_data_mux, data);
+
+	v = readl(J7_MAIN_CTRL_MMR + J7_DSS_DISPC0_CLKSEL3);
+
+	/* Output 1 if bits 2 and 0 are set, 0 otherwise */
+	v = (v & 0x5UL) == 0x5UL;
+
+	return (v < mux->n && mux->parents[v].div) ? &mux->parents[v] : NULL;
+}
+
+static sbool clk_mux_j7_dpi_0_pclk_set_parent(struct clk *clk	TRACE_ONLY,
+					      u8		new_parent)
+{
+	u32 v;
+	sbool ret;
+	s32 err;
+
+	v = readl(J7_MAIN_CTRL_MMR + J7_DSS_DISPC0_CLKSEL3);
+
+	if ((v & 0x4UL) == 0UL) {
+		if (new_parent == 1U) {
+			/* Cannot switch to 1 with out changing other clocks */
+			ret = SFALSE;
+		} else if (new_parent == 0U) {
+			/* Already on parent 0, nothing to do */
+			ret = STRUE;
+		} else {
+			ret = SFALSE;
+		}
+	} else {
+		if (new_parent == 1U) {
+			v |= 0x1U;
+			ret = STRUE;
+		} else if (new_parent == 0U) {
+			v &= ~0x1U;
+			ret = STRUE;
+		} else {
+			ret = SFALSE;
+		}
+		if (ret) {
+			err = pm_writel_verified(v, J7_MAIN_CTRL_MMR + J7_DSS_DISPC0_CLKSEL3);
+			if (err != SUCCESS) {
+				ret = SFALSE;
+			}
+		}
+	}
+
+	if (ret) {
+		pm_trace(TRACE_PM_ACTION_CLOCK_SET_PARENT,
+			 ((new_parent << TRACE_PM_VAL_CLOCK_VAL_SHIFT) &
+			  TRACE_PM_VAL_CLOCK_VAL_MASK) |
+			 ((clk_id(clk) << TRACE_PM_VAL_CLOCK_ID_SHIFT) &
+			  TRACE_PM_VAL_CLOCK_ID_MASK));
+	}
+
+	return ret;
+}
+
+const struct clk_drv_mux clk_drv_mux_j7_dpi_0_pclk = {
+	.get_parent		= clk_mux_j7_dpi_0_pclk_get_parent,
+	.set_parent		= clk_mux_j7_dpi_0_pclk_set_parent,
+};
+
+static const struct clk_parent *clk_mux_j7_dpi_2_pclk_get_parent(struct clk *clk)
+{
+	const struct clk_data *clk_data = clk_get_data(clk);
+	const struct clk_data_mux *mux;
+	u32 v;
+
+	mux = container_of(clk_data->data, const struct clk_data_mux, data);
+
+	v = readl(J7_MAIN_CTRL_MMR + J7_DSS_DISPC0_CLKSEL3);
+
+	/*
+	 * Our mux is expanded to include the parent mux, input 0 is the
+	 * parent mux. We shift input 1 to input 2
+	 */
+	if ((v & 0x6UL) == 0x6UL) {
+		/* Output 1 if bits 2 and 1 are set, 0 otherwise */
+		v = 2UL;
+	} else {
+		v = readl(J7_MAIN_CTRL_MMR + J7_DSS_DISPC0_CLKSEL2);
+		if ((v & 0x1UL) == 0x1UL) {
+			v = 1UL;
+		} else {
+			v = 0UL;
+		}
+	}
+
+	return (v < mux->n && mux->parents[v].div) ? &mux->parents[v] : NULL;
+}
+
+static sbool clk_mux_j7_dpi_2_pclk_set_parent(struct clk *clk	TRACE_ONLY,
+					      u8		new_parent)
+{
+	u32 v;
+	sbool ret = SFALSE;
+	sbool switch_parent = SFALSE;
+	s32 err;
+
+	v = readl(J7_MAIN_CTRL_MMR + J7_DSS_DISPC0_CLKSEL3);
+
+	if ((v & 0x4UL) == 0UL) {
+		if (new_parent == 2U) {
+			/* Cannot switch to 2 with out changing other clocks */
+			ret = SFALSE;
+		} else if (new_parent == 0U || new_parent == 1U) {
+			ret = STRUE;
+			switch_parent = STRUE;
+		} else {
+			ret = SFALSE;
+		}
+	} else {
+		if (new_parent == 2U) {
+			v |= 0x2U;
+			ret = STRUE;
+		} else if (new_parent == 0U || new_parent == 1U) {
+			v &= ~0x2U;
+			ret = STRUE;
+			switch_parent = STRUE;
+		} else {
+			ret = SFALSE;
+		}
+		if (ret) {
+			err = pm_writel_verified(v, J7_MAIN_CTRL_MMR + J7_DSS_DISPC0_CLKSEL3);
+			if (err != SUCCESS) {
+				ret = SFALSE;
+			}
+		}
+	}
+
+	if (switch_parent && ret) {
+		v = readl(J7_MAIN_CTRL_MMR + J7_DSS_DISPC0_CLKSEL2);
+		if (new_parent == 1U) {
+			v |= 1UL;
+		} else {
+			v &= ~1UL;
+		}
+		err = pm_writel_verified(v, J7_MAIN_CTRL_MMR + J7_DSS_DISPC0_CLKSEL2);
+		if (err != SUCCESS) {
+			ret = SFALSE;
+		}
+	}
+
+	if (ret) {
+		pm_trace(TRACE_PM_ACTION_CLOCK_SET_PARENT,
+			 ((new_parent << TRACE_PM_VAL_CLOCK_VAL_SHIFT) &
+			  TRACE_PM_VAL_CLOCK_VAL_MASK) |
+			 ((clk_id(clk) << TRACE_PM_VAL_CLOCK_ID_SHIFT) &
+			  TRACE_PM_VAL_CLOCK_ID_MASK));
+	}
+
+	return ret;
+}
+
+const struct clk_drv_mux clk_drv_mux_j7_dpi_2_pclk = {
+	.get_parent		= clk_mux_j7_dpi_2_pclk_get_parent,
+	.set_parent		= clk_mux_j7_dpi_2_pclk_set_parent,
+};
+
+static const struct clk_parent *clk_mux_j7_dpi_3_pclk_get_parent(struct clk *clk)
+{
+	const struct clk_data *clk_data = clk_get_data(clk);
+	const struct clk_data_mux *mux;
+	const struct clk_data_mux_reg *reg;
+	u32 v;
+
+	mux = container_of(clk_data->data, const struct clk_data_mux, data);
+	reg = container_of(mux, const struct clk_data_mux_reg, data_mux);
+
+	v = readl(reg->reg);
+	v >>= reg->bit;
+	v &= (1 << ilog32(mux->n - 1)) - 1;
+
+	/* Output 4 if bits 2 is set, low bits otherwise */
+	if (v > 4UL) {
+		v = 4UL;
+	}
+
+	return (v < mux->n && mux->parents[v].div) ? &mux->parents[v] : NULL;
+}
+
+static sbool clk_mux_j7_dpi_3_pclk_set_parent(struct clk *clk, u8 new_parent)
+{
+	const struct clk_data *clk_data = clk_get_data(clk);
+	const struct clk_data_mux *mux;
+	const struct clk_data_mux_reg *reg;
+	u32 v;
+	u32 curr_parent;
+	sbool ret;
+	sbool change = STRUE;
+	u32 mask;
+	s32 err;
+
+	mux = container_of(clk_data->data, const struct clk_data_mux, data);
+	reg = container_of(mux, const struct clk_data_mux_reg, data_mux);
+
+	mask = ((1U << ilog32(mux->n - 1U)) - 1U) << reg->bit;
+	v = readl(reg->reg);
+	curr_parent = (v & mask) >> reg->bit;
+
+	if (new_parent < 4U) {
+		if ((curr_parent & 0x4UL) == 0UL) {
+			/*
+			 * Original parent is 0-3, new parent is 0-3
+			 * No special action needed
+			 */
+			ret = STRUE;
+		} else {
+			/* Original parent is 4 */
+			if ((curr_parent & 0x3UL) != 0UL) {
+				/*
+				 * dpi_0 or dpi_2 is using high clk, cannot
+				 * switch without changing their setting
+				 */
+				ret = SFALSE;
+			} else {
+				/*
+				 * We can switch to 0-3 without changing
+				 * the dpi_0/dpi_2 output since they are both
+				 * already low.
+				 */
+				ret = STRUE;
+			}
+		}
+	} else if (new_parent == 4U) {
+		if ((curr_parent & 0x4UL) == 0UL) {
+			/*
+			 * Original parent is 0-3. Select proper mux parennt 4.
+			 * with the dpi_0/dpi_2 bits cleared to keep them from
+			 * switching.
+			 */
+			ret = STRUE;
+		} else {
+			/*
+			 * Original parent is 4, new parent is 4. Extra mux
+			 * bits contain state of dpi_0/dpi_2, don't change
+			 * them.
+			 */
+			change = SFALSE;
+			ret = STRUE;
+		}
+	} else {
+		/* Invalid setting */
+		ret = SFALSE;
+	}
+
+	if (ret && change) {
+		v &= ~mask;
+		v |= new_parent << reg->bit;
+		err = pm_writel_verified(v, reg->reg);
+		if (err != SUCCESS) {
+			ret = SFALSE;
+		}
+		pm_trace(TRACE_PM_ACTION_CLOCK_SET_PARENT,
+			 ((new_parent << TRACE_PM_VAL_CLOCK_VAL_SHIFT) &
+			  TRACE_PM_VAL_CLOCK_VAL_MASK) |
+			 ((clk_id(clk) << TRACE_PM_VAL_CLOCK_ID_SHIFT) &
+			  TRACE_PM_VAL_CLOCK_ID_MASK));
+	}
+
+	return ret;
+}
+
+const struct clk_drv_mux clk_drv_mux_reg_j7_dpi_3_pclk = {
+	.get_parent		= clk_mux_j7_dpi_3_pclk_get_parent,
+	.set_parent		= clk_mux_j7_dpi_3_pclk_set_parent,
+};
+
+
+static const struct clk_parent *clk_mux_j7_dpi_1_clk_out_get_parent(struct clk *clk)
+{
+	const struct clk_data *clk_data = clk_get_data(clk);
+	const struct clk_data_mux *mux;
+	u32 v;
+
+	mux = container_of(clk_data->data, const struct clk_data_mux, data);
+
+	v = readl(J7_MAIN_CTRL_MMR + J7_DSS_DISPC0_CLKSEL3);
+
+	/*
+	 * Our mux is expanded to include the parent mux, input 0 is the
+	 * parent mux. We shift input 1 to input 2
+	 */
+	if ((v & 0x6UL) == 0x6UL) {
+		/* Output 1 if bits 2 and 1 are set, 0 otherwise */
+		v = 2UL;
+	} else {
+		v = readl(J7_MAIN_CTRL_MMR + J7_DSS_DISPC0_CLKSEL2);
+		if ((v & 0x1UL) == 0x1UL) {
+			v = 1UL;
+		} else {
+			v = 0UL;
+		}
+	}
+
+	return (v < mux->n && mux->parents[v].div) ? &mux->parents[v] : NULL;
+}
+
+static sbool clk_mux_j7_dpi_1_clk_out_set_parent(struct clk *clk	TRACE_ONLY,
+						 u8			new_parent)
+{
+	u32 v;
+	sbool ret = SFALSE;
+	sbool switch_parent = SFALSE;
+	s32 err;
+
+	v = readl(J7_MAIN_CTRL_MMR + J7_DSS_DISPC0_CLKSEL3);
+
+	if ((v & 0x4UL) == 0UL) {
+		if (new_parent == 2U) {
+			/* Cannot switch to 2 with out changing other clocks */
+			ret = SFALSE;
+		} else if (new_parent == 0U || new_parent == 1U) {
+			ret = STRUE;
+			switch_parent = STRUE;
+		} else {
+			ret = SFALSE;
+		}
+	} else {
+		if (new_parent == 2U) {
+			v |= 0x2U;
+			ret = STRUE;
+		} else if (new_parent == 0U || new_parent == 1U) {
+			v &= ~0x2U;
+			ret = STRUE;
+			switch_parent = STRUE;
+		} else {
+			ret = SFALSE;
+		}
+		if (ret) {
+			err = pm_writel_verified(v, J7_MAIN_CTRL_MMR + J7_DSS_DISPC0_CLKSEL3);
+			if (err != SUCCESS) {
+				ret = SFALSE;
+			}
+		}
+	}
+
+	if (switch_parent && ret) {
+		v = readl(J7_MAIN_CTRL_MMR + J7_DSS_DISPC0_CLKSEL2);
+		if (new_parent == 1U) {
+			v |= 1UL;
+		} else {
+			v &= ~1UL;
+		}
+		err = pm_writel_verified(v, J7_MAIN_CTRL_MMR + J7_DSS_DISPC0_CLKSEL2);
+		if (err != SUCCESS) {
+			ret = SFALSE;
+		}
+	}
+
+	if (ret) {
+		pm_trace(TRACE_PM_ACTION_CLOCK_SET_PARENT,
+			 ((new_parent << TRACE_PM_VAL_CLOCK_VAL_SHIFT) &
+			  TRACE_PM_VAL_CLOCK_VAL_MASK) |
+			 ((clk_id(clk) << TRACE_PM_VAL_CLOCK_ID_SHIFT) &
+			  TRACE_PM_VAL_CLOCK_ID_MASK));
+	}
+
+	return ret;
+}
+
+const struct clk_drv_mux clk_drv_mux_j7_dpi_1_clk_out = {
+	.get_parent		= clk_mux_j7_dpi_1_clk_out_get_parent,
+	.set_parent		= clk_mux_j7_dpi_1_clk_out_set_parent,
+};
---
+++ rm/drivers/ra/soc/j784s4/ra_cfg.c
@@ -0,0 +1,479 @@
+/*
+ * Ring Accelerator
+ *
+ * Ring accelerator driver instance data definition
+ *
+ * Data version: 220421_082009
+ *
+ * Copyright (C) 2022 Texas Instruments Incorporated - http://www.ti.com/
+ * ALL RIGHTS RESERVED
+ */
+
+#include <types/short_types.h>
+#include <types/array_size.h>
+#include <types/sbool.h>
+#include <soc/rm_mmr.h>
+#include <devices.h>
+#include <soc/j784s4/resasg_types.h>
+#include <soc/j784s4/soc_devgrps.h>
+
+#include "../../include/ra_inst.h"
+#include "ra_cfg.h"
+
+static const struct rm_mmr mmr_NAVSS0_BCDMA_0_BCDMA0_CFG_GCFG = {
+	.fwl_id		= RM_MMR_FWL_INVALID,
+	.fwl_ch_start	= 0U,
+	.fwl_ch_end	= 0U,
+	.base		= 0x311a0000U,
+};
+
+static const struct rm_mmr mmr_NAVSS0_BCDMA_0_BCDMA0_CFG_RING = {
+	.fwl_id		= RM_MMR_FWL_INVALID,
+	.fwl_ch_start	= 0U,
+	.fwl_ch_end	= 0U,
+	.base		= 0x35900000U,
+};
+
+static const struct rm_mmr mmr_NAVSS0_BCDMA_0_BCDMA0_CFG_RINGRT = {
+	.fwl_id		= RM_MMR_FWL_INVALID,
+	.fwl_ch_start	= 0U,
+	.fwl_ch_end	= 0U,
+	.base		= 0x35e00000U,
+};
+
+static const struct ra_ring_type ring_types_NAVSS0_BCDMA_0[2] = {
+	{
+		.start = 0U,
+		.end = 15U,
+		.type = RA_DMSS_RING,
+		.utype = RESASG_UTYPE(J784S4_DEV_NAVSS0_BCDMA_0, RESASG_SUBTYPE_BCDMA_RING_SPLIT_TR_TX_CHAN),
+	},
+	{
+		.start = 16U,
+		.end = 47U,
+		.type = RA_DMSS_RING,
+		.utype = RESASG_UTYPE(J784S4_DEV_NAVSS0_BCDMA_0, RESASG_SUBTYPE_BCDMA_RING_SPLIT_TR_RX_CHAN),
+	},
+};
+
+static const struct rm_mmr mmr_NAVSS0_RINGACC_0_UDMASS_RINGACC0_GCFG = {
+	.fwl_id		= RM_MMR_FWL_INVALID,
+	.fwl_ch_start	= 0U,
+	.fwl_ch_end	= 0U,
+	.base		= 0x31160000U,
+};
+
+static const struct rm_mmr mmr_NAVSS0_RINGACC_0_UDMASS_RINGACC0_SRC_FIFOS = {
+	.fwl_id		= 4128U,
+	.fwl_ch_start	= 0U,
+	.fwl_ch_end	= 1023U,
+	.base		= 0x38000000U,
+};
+
+static const struct rm_mmr mmr_NAVSS0_RINGACC_0_UDMASS_RINGACC0_CFG_MON = {
+	.fwl_id		= 4224U,
+	.fwl_ch_start	= 1024U,
+	.fwl_ch_end	= 1055U,
+	.base		= 0x32000000U,
+};
+
+static const struct rm_mmr mmr_NAVSS0_RINGACC_0_UDMASS_RINGACC0_CFG = {
+	.fwl_id		= RM_MMR_FWL_INVALID,
+	.fwl_ch_start	= 0U,
+	.fwl_ch_end	= 0U,
+	.base		= 0x31080000U,
+};
+
+static const struct rm_mmr mmr_NAVSS0_RINGACC_0_UDMASS_RINGACC0_CFG_RT = {
+	.fwl_id		= 4224U,
+	.fwl_ch_start	= 0U,
+	.fwl_ch_end	= 1023U,
+	.base		= 0x3c000000U,
+};
+
+static const struct ra_ring_type ring_types_NAVSS0_RINGACC_0[9] = {
+	{
+		.start = 0U,
+		.end = 1U,
+		.type = RA_STANDARD_RING,
+		.utype = RESASG_UTYPE(J784S4_DEV_NAVSS0_RINGACC_0, RESASG_SUBTYPE_RA_UDMAP_TX_UH),
+	},
+	{
+		.start = 2U,
+		.end = 3U,
+		.type = RA_STANDARD_RING,
+		.utype = RESASG_UTYPE(J784S4_DEV_NAVSS0_RINGACC_0, RESASG_SUBTYPE_RA_UDMAP_TX_H),
+	},
+	{
+		.start = 4U,
+		.end = 84U,
+		.type = RA_STANDARD_RING,
+		.utype = RESASG_UTYPE(J784S4_DEV_NAVSS0_RINGACC_0, RESASG_SUBTYPE_RA_UDMAP_TX),
+	},
+	{
+		.start = 85U,
+		.end = 340U,
+		.type = RA_STANDARD_RING,
+		.utype = RESASG_UTYPE(J784S4_DEV_NAVSS0_RINGACC_0, RESASG_SUBTYPE_RA_UDMAP_TX_EXT),
+	},
+	{
+		.start = 341U,
+		.end = 342U,
+		.type = RA_STANDARD_RING,
+		.utype = RESASG_UTYPE(J784S4_DEV_NAVSS0_RINGACC_0, RESASG_SUBTYPE_RA_UDMAP_RX_UH),
+	},
+	{
+		.start = 343U,
+		.end = 344U,
+		.type = RA_STANDARD_RING,
+		.utype = RESASG_UTYPE(J784S4_DEV_NAVSS0_RINGACC_0, RESASG_SUBTYPE_RA_UDMAP_RX_H),
+	},
+	{
+		.start = 345U,
+		.end = 422U,
+		.type = RA_STANDARD_RING,
+		.utype = RESASG_UTYPE(J784S4_DEV_NAVSS0_RINGACC_0, RESASG_SUBTYPE_RA_UDMAP_RX),
+	},
+	{
+		.start = 423U,
+		.end = 767U,
+		.type = RA_STANDARD_RING,
+		.utype = RESASG_UTYPE(J784S4_DEV_NAVSS0_RINGACC_0, RESASG_SUBTYPE_RA_GP),
+	},
+	{
+		.start = 878U,
+		.end = 1023U,
+		.type = RA_STANDARD_RING,
+		.utype = RESASG_UTYPE(J784S4_DEV_NAVSS0_RINGACC_0, RESASG_SUBTYPE_RA_GP),
+	},
+};
+
+static const struct rm_mmr mmr_MCU_NAVSS0_RINGACC0_UDMASS_RINGACC0_CFG_GCFG = {
+	.fwl_id		= RM_MMR_FWL_INVALID,
+	.fwl_ch_start	= 0U,
+	.fwl_ch_end	= 0U,
+	.base		= 0x285d0000U,
+};
+
+static const struct rm_mmr mmr_MCU_NAVSS0_RINGACC0_UDMASS_RINGACC0_FIFOS = {
+	.fwl_id		= 6272U,
+	.fwl_ch_start	= 0U,
+	.fwl_ch_end	= 285U,
+	.base		= 0x2b000000U,
+};
+
+static const struct rm_mmr mmr_MCU_NAVSS0_RINGACC0_UDMASS_RINGACC0_CFG_MON = {
+	.fwl_id		= 6160U,
+	.fwl_ch_start	= 286U,
+	.fwl_ch_end	= 317U,
+	.base		= 0x2a280000U,
+};
+
+static const struct rm_mmr mmr_MCU_NAVSS0_RINGACC0_UDMASS_RINGACC0_CFG = {
+	.fwl_id		= RM_MMR_FWL_INVALID,
+	.fwl_ch_start	= 0U,
+	.fwl_ch_end	= 0U,
+	.base		= 0x28440000U,
+};
+
+static const struct rm_mmr mmr_MCU_NAVSS0_RINGACC0_UDMASS_RINGACC0_CFG_RT = {
+	.fwl_id		= 6160U,
+	.fwl_ch_start	= 0U,
+	.fwl_ch_end	= 285U,
+	.base		= 0x2b800000U,
+};
+
+static const struct ra_ring_type ring_types_MCU_NAVSS0_RINGACC0[5] = {
+	{
+		.start = 0U,
+		.end = 1U,
+		.type = RA_STANDARD_RING,
+		.utype = RESASG_UTYPE(J784S4_DEV_MCU_NAVSS0_RINGACC0, RESASG_SUBTYPE_RA_UDMAP_TX_H),
+	},
+	{
+		.start = 2U,
+		.end = 47U,
+		.type = RA_STANDARD_RING,
+		.utype = RESASG_UTYPE(J784S4_DEV_MCU_NAVSS0_RINGACC0, RESASG_SUBTYPE_RA_UDMAP_TX),
+	},
+	{
+		.start = 48U,
+		.end = 49U,
+		.type = RA_STANDARD_RING,
+		.utype = RESASG_UTYPE(J784S4_DEV_MCU_NAVSS0_RINGACC0, RESASG_SUBTYPE_RA_UDMAP_RX_H),
+	},
+	{
+		.start = 50U,
+		.end = 95U,
+		.type = RA_STANDARD_RING,
+		.utype = RESASG_UTYPE(J784S4_DEV_MCU_NAVSS0_RINGACC0, RESASG_SUBTYPE_RA_UDMAP_RX),
+	},
+	{
+		.start = 96U,
+		.end = 255U,
+		.type = RA_STANDARD_RING,
+		.utype = RESASG_UTYPE(J784S4_DEV_MCU_NAVSS0_RINGACC0, RESASG_SUBTYPE_RA_GP),
+	},
+};
+
+/*
+ * Resources used by ROM which must have their OES registers reset
+ */
+static struct ra_used_mapping rom_usage_MCU_NAVSS0_RINGACC0[13U] = {
+	{
+		.ring = 0U,
+		.cleared = SFALSE,
+	},
+	{
+		.ring = 1U,
+		.cleared = SFALSE,
+	},
+	{
+		.ring = 2U,
+		.cleared = SFALSE,
+	},
+	{
+		.ring = 3U,
+		.cleared = SFALSE,
+	},
+	{
+		.ring = 4U,
+		.cleared = SFALSE,
+	},
+	{
+		.ring = 52U,
+		.cleared = SFALSE,
+	},
+	{
+		.ring = 100U,
+		.cleared = SFALSE,
+	},
+	{
+		.ring = 101U,
+		.cleared = SFALSE,
+	},
+	{
+		.ring = 102U,
+		.cleared = SFALSE,
+	},
+	{
+		.ring = 256U,
+		.cleared = SFALSE,
+	},
+	{
+		.ring = 257U,
+		.cleared = SFALSE,
+	},
+	{
+		.ring = 258U,
+		.cleared = SFALSE,
+	},
+	{
+		.ring = 259U,
+		.cleared = SFALSE,
+	},
+};
+
+static const struct rm_mmr mmr_MCU_SA3_SS0_PKTDMA_0_PKTDMA_CFG_GCFG = {
+	.fwl_id		= RM_MMR_FWL_INVALID,
+	.fwl_ch_start	= 0U,
+	.fwl_ch_end	= 0U,
+	.base		= 0x44910000U,
+};
+
+static const struct rm_mmr mmr_MCU_SA3_SS0_PKTDMA_0_PKTDMA_CFG_RING = {
+	.fwl_id		= RM_MMR_FWL_INVALID,
+	.fwl_ch_start	= 0U,
+	.fwl_ch_end	= 0U,
+	.base		= 0x4491a000U,
+};
+
+static const struct rm_mmr mmr_MCU_SA3_SS0_PKTDMA_0_PKTDMA_CFG_RINGRT = {
+	.fwl_id		= RM_MMR_FWL_INVALID,
+	.fwl_ch_start	= 0U,
+	.fwl_ch_end	= 0U,
+	.base		= 0x44940000U,
+};
+
+static const struct ra_ring_type ring_types_MCU_SA3_SS0_PKTDMA_0[6] = {
+	{
+		.start = 0U,
+		.end = 7U,
+		.type = RA_DMSS_RING,
+		.utype = RESASG_UTYPE(J784S4_DEV_MCU_SA3_SS0_PKTDMA_0, RESASG_SUBTYPE_PKTDMA_RING_SAUL_TX_0_CHAN),
+	},
+	{
+		.start = 8U,
+		.end = 15U,
+		.type = RA_DMSS_RING,
+		.utype = RESASG_UTYPE(J784S4_DEV_MCU_SA3_SS0_PKTDMA_0, RESASG_SUBTYPE_PKTDMA_RING_SAUL_TX_1_CHAN),
+	},
+	{
+		.start = 16U,
+		.end = 23U,
+		.type = RA_DMSS_RING,
+		.utype = RESASG_UTYPE(J784S4_DEV_MCU_SA3_SS0_PKTDMA_0, RESASG_SUBTYPE_PKTDMA_RING_SAUL_RX_0_CHAN),
+	},
+	{
+		.start = 16U,
+		.end = 23U,
+		.type = RA_DMSS_RING,
+		.utype = RESASG_UTYPE(J784S4_DEV_MCU_SA3_SS0_PKTDMA_0, RESASG_SUBTYPE_PKTDMA_RING_SAUL_RX_1_CHAN),
+	},
+	{
+		.start = 24U,
+		.end = 31U,
+		.type = RA_DMSS_RING,
+		.utype = RESASG_UTYPE(J784S4_DEV_MCU_SA3_SS0_PKTDMA_0, RESASG_SUBTYPE_PKTDMA_RING_SAUL_RX_2_CHAN),
+	},
+	{
+		.start = 24U,
+		.end = 31U,
+		.type = RA_DMSS_RING,
+		.utype = RESASG_UTYPE(J784S4_DEV_MCU_SA3_SS0_PKTDMA_0, RESASG_SUBTYPE_PKTDMA_RING_SAUL_RX_3_CHAN),
+	},
+};
+
+static const struct rm_mmr mmr_MCU_SA3_SS0_RINGACC_0_IPCSS_RINGACC_CFG_GCFG = {
+	.fwl_id		= RM_MMR_FWL_INVALID,
+	.fwl_ch_start	= 0U,
+	.fwl_ch_end	= 0U,
+	.base		= 0x44805000U,
+};
+
+static const struct rm_mmr mmr_MCU_SA3_SS0_RINGACC_0_IPCSS_RINGACC_CFG = {
+	.fwl_id		= RM_MMR_FWL_INVALID,
+	.fwl_ch_start	= 0U,
+	.fwl_ch_end	= 0U,
+	.base		= 0x448c0000U,
+};
+
+static const struct rm_mmr mmr_MCU_SA3_SS0_RINGACC_0_IPCSS_RINGACC_CFG_RT = {
+	.fwl_id		= RM_MMR_FWL_INVALID,
+	.fwl_ch_start	= 0U,
+	.fwl_ch_end	= 0U,
+	.base		= 0x44c00000U,
+};
+
+static const struct ra_ring_type ring_types_MCU_SA3_SS0_RINGACC_0[0] = {
+};
+
+struct ra_instance ra_inst[] = {
+	{
+		.id = J784S4_DEV_NAVSS0_BCDMA_0,
+		.root_id = J784S4_DEV_NAVSS0,
+		.initialized = SFALSE,
+		.devgrp = SOC_DEVGRP_J784S4_MAIN,
+		.gcfg = &mmr_NAVSS0_BCDMA_0_BCDMA0_CFG_GCFG,
+		.cfg = &mmr_NAVSS0_BCDMA_0_BCDMA0_CFG_RING,
+		.rt = &mmr_NAVSS0_BCDMA_0_BCDMA0_CFG_RINGRT,
+		.mon = NULL,
+		.fifos = NULL,
+		.n_ring = 0U,
+		.ring_oes_offset = 0U,
+		.ring_types = &ring_types_NAVSS0_BCDMA_0[0U],
+		.n_ring_type = ARRAY_SIZE(ring_types_NAVSS0_BCDMA_0),
+		.err_evt_index = RA_GCFG_ERROR_EVT_INDEX,
+		.err_evt_utype = RESASG_UTYPE(J784S4_DEV_NAVSS0_BCDMA_0, RESASG_SUBTYPE_RA_ERROR_OES),
+		.virtid_utype = 0U,
+		.n_ring_mon = 0U,
+		.ring_mon_utype = 0U,
+		.ring_mon_oes_offset = 0U,
+		.rom_usage = NULL,
+		.n_rom_usage = 0U,
+	},
+	{
+		.id = J784S4_DEV_NAVSS0_RINGACC_0,
+		.root_id = J784S4_DEV_NAVSS0,
+		.initialized = SFALSE,
+		.devgrp = SOC_DEVGRP_J784S4_MAIN,
+		.gcfg = &mmr_NAVSS0_RINGACC_0_UDMASS_RINGACC0_GCFG,
+		.cfg = &mmr_NAVSS0_RINGACC_0_UDMASS_RINGACC0_CFG,
+		.rt = &mmr_NAVSS0_RINGACC_0_UDMASS_RINGACC0_CFG_RT,
+		.mon = &mmr_NAVSS0_RINGACC_0_UDMASS_RINGACC0_CFG_MON,
+		.fifos = &mmr_NAVSS0_RINGACC_0_UDMASS_RINGACC0_SRC_FIFOS,
+		.n_ring = RA_NAVSS0_RINGACC_0_MAX_INDEX,
+		.ring_oes_offset = RA_NAVSS0_RINGACC_0_RING_OES_EVT_INDEX_OFFSET,
+		.ring_types = &ring_types_NAVSS0_RINGACC_0[0U],
+		.n_ring_type = ARRAY_SIZE(ring_types_NAVSS0_RINGACC_0),
+		.err_evt_index = RA_GCFG_ERROR_EVT_INDEX,
+		.err_evt_utype = RESASG_UTYPE(J784S4_DEV_NAVSS0_RINGACC_0, RESASG_SUBTYPE_RA_ERROR_OES),
+		.virtid_utype = RESASG_UTYPE(J784S4_DEV_NAVSS0_RINGACC_0, RESASG_SUBTYPE_RA_VIRTID),
+		.n_ring_mon = RA_NAVSS0_RINGACC_0_MONITOR_MAX_INDEX,
+		.ring_mon_utype = RESASG_UTYPE(J784S4_DEV_NAVSS0_RINGACC_0, RESASG_SUBTYPE_RA_MONITORS),
+		.ring_mon_oes_offset = RA_MON_OES_EVT_INDEX_OFFSET,
+		.rom_usage = NULL,
+		.n_rom_usage = 0U,
+	},
+	{
+		.id = J784S4_DEV_MCU_NAVSS0_RINGACC0,
+		.root_id = J784S4_DEV_MCU_NAVSS0,
+		.initialized = SFALSE,
+		.devgrp = SOC_DEVGRP_J784S4_MCU_WAKEUP | SOC_DEVGRP_J784S4_TIFS_INTERNAL,
+		.gcfg = &mmr_MCU_NAVSS0_RINGACC0_UDMASS_RINGACC0_CFG_GCFG,
+		.cfg = &mmr_MCU_NAVSS0_RINGACC0_UDMASS_RINGACC0_CFG,
+		.rt = &mmr_MCU_NAVSS0_RINGACC0_UDMASS_RINGACC0_CFG_RT,
+		.mon = &mmr_MCU_NAVSS0_RINGACC0_UDMASS_RINGACC0_CFG_MON,
+		.fifos = &mmr_MCU_NAVSS0_RINGACC0_UDMASS_RINGACC0_FIFOS,
+		.n_ring = RA_MCU_NAVSS0_RINGACC0_MAX_INDEX,
+		.ring_oes_offset = RA_MCU_NAVSS0_RINGACC0_RING_OES_EVT_INDEX_OFFSET,
+		.ring_types = &ring_types_MCU_NAVSS0_RINGACC0[0U],
+		.n_ring_type = ARRAY_SIZE(ring_types_MCU_NAVSS0_RINGACC0),
+		.err_evt_index = RA_GCFG_ERROR_EVT_INDEX,
+		.err_evt_utype = RESASG_UTYPE(J784S4_DEV_MCU_NAVSS0_RINGACC0, RESASG_SUBTYPE_RA_ERROR_OES),
+		.virtid_utype = RESASG_UTYPE(J784S4_DEV_MCU_NAVSS0_RINGACC0, RESASG_SUBTYPE_RA_VIRTID),
+		.n_ring_mon = RA_MCU_NAVSS0_RINGACC0_MONITOR_MAX_INDEX,
+		.ring_mon_utype = RESASG_UTYPE(J784S4_DEV_MCU_NAVSS0_RINGACC0, RESASG_SUBTYPE_RA_MONITORS),
+		.ring_mon_oes_offset = RA_MON_OES_EVT_INDEX_OFFSET,
+		.rom_usage = &rom_usage_MCU_NAVSS0_RINGACC0[0U],
+		.n_rom_usage = ARRAY_SIZE(rom_usage_MCU_NAVSS0_RINGACC0),
+	},
+	{
+		.id = J784S4_DEV_MCU_SA3_SS0_PKTDMA_0,
+		.root_id = J784S4_DEV_MCU_SA3_SS0,
+		.initialized = SFALSE,
+		.devgrp = SOC_DEVGRP_J784S4_TIFS_INTERNAL,
+		.gcfg = &mmr_MCU_SA3_SS0_PKTDMA_0_PKTDMA_CFG_GCFG,
+		.cfg = &mmr_MCU_SA3_SS0_PKTDMA_0_PKTDMA_CFG_RING,
+		.rt = &mmr_MCU_SA3_SS0_PKTDMA_0_PKTDMA_CFG_RINGRT,
+		.mon = NULL,
+		.fifos = NULL,
+		.n_ring = 0U,
+		.ring_oes_offset = 0U,
+		.ring_types = &ring_types_MCU_SA3_SS0_PKTDMA_0[0U],
+		.n_ring_type = ARRAY_SIZE(ring_types_MCU_SA3_SS0_PKTDMA_0),
+		.err_evt_index = RA_GCFG_ERROR_EVT_INDEX,
+		.err_evt_utype = RESASG_UTYPE(J784S4_DEV_MCU_SA3_SS0_PKTDMA_0, RESASG_SUBTYPE_RA_ERROR_OES),
+		.virtid_utype = 0U,
+		.n_ring_mon = 0U,
+		.ring_mon_utype = 0U,
+		.ring_mon_oes_offset = 0U,
+		.rom_usage = NULL,
+		.n_rom_usage = 0U,
+	},
+	{
+		.id = J784S4_DEV_MCU_SA3_SS0_RINGACC_0,
+		.root_id = J784S4_DEV_MCU_SA3_SS0,
+		.initialized = SFALSE,
+		.devgrp = SOC_DEVGRP_J784S4_TIFS_INTERNAL,
+		.gcfg = &mmr_MCU_SA3_SS0_RINGACC_0_IPCSS_RINGACC_CFG_GCFG,
+		.cfg = &mmr_MCU_SA3_SS0_RINGACC_0_IPCSS_RINGACC_CFG,
+		.rt = &mmr_MCU_SA3_SS0_RINGACC_0_IPCSS_RINGACC_CFG_RT,
+		.mon = NULL,
+		.fifos = NULL,
+		.n_ring = RA_MCU_SA3_SS0_RINGACC_0_MAX_INDEX,
+		.ring_oes_offset = RA_MCU_SA3_SS0_RINGACC_0_RING_OES_EVT_INDEX_OFFSET,
+		.ring_types = &ring_types_MCU_SA3_SS0_RINGACC_0[0U],
+		.n_ring_type = ARRAY_SIZE(ring_types_MCU_SA3_SS0_RINGACC_0),
+		.err_evt_index = RA_GCFG_ERROR_EVT_INDEX,
+		.err_evt_utype = RESASG_UTYPE(J784S4_DEV_MCU_SA3_SS0_RINGACC_0, RESASG_SUBTYPE_RA_ERROR_OES),
+		.virtid_utype = RESASG_UTYPE(J784S4_DEV_MCU_SA3_SS0_RINGACC_0, RESASG_SUBTYPE_RA_VIRTID),
+		.n_ring_mon = 0U,
+		.ring_mon_utype = 0U,
+		.ring_mon_oes_offset = 0U,
+		.rom_usage = NULL,
+		.n_rom_usage = 0U,
+	},
+};
+
+const u32 ra_inst_count = ARRAY_SIZE(ra_inst);
