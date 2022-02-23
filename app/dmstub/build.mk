c2a3aedcb486d423eed10e3ae360ec4e7d5c4eea
Dave Gerlach <d-gerlach@ti.com>
2022-02-23 04:19:41 +0000

    build: Introduce build infrastructure and add dmstub app
    This commit is temporary until PDK/RTOS dependency is sorted out.
    
    Introduce top level Makefile and build rules to allow generation of
    binaries directly inside rm_pm_hal, and add "dmstub" under app/ with
    rules and support code to generate a standalone binary.
    
    The CONFIG_LPM_DM_STUB configuration option causes this binary to get
    generated, and the startup code introduced here provides the following:
    
         * Entry point at the address provided at CONFIG_LNK_NSPS_BASE.
         * Linker script configured to locate all data and code in region
           defined by CONFIG_LNK_NSPS_BASE and CONFIG_LNK_NSPS_LEN.
         * Reset of stack pointer to address in local memory.
         * Zero-ing of stub local bss section.
         * Jump to dm_stub_entry in C code from startup code.
    
    Signed-off-by: Dave Gerlach <d-gerlach@ti.com>

---
+++ app/dmstub/build.mk
@@ -0,0 +1,5 @@
+# Copyright (C) 2021 Texas Instruments Incorporated - http://www.ti.com/
+# ALL RIGHTS RESERVED
+
+prepare_headers-y += $(srctree)/include/config.h
+obj-y += entry.o startup.o
--- lpm/build.mk
+++ lpm/build.mk
@@ -1,1 +1,3 @@
+TARGET_SOC = $(shell echo $(CONFIG_SOC_STRING))
+
 obj-y += main.o
