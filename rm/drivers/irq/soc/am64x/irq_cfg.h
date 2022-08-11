f6fdc9dafeb717ff4b2b2a69153c1339fb6ab737
Vishal Mahaveer <vishalm@ti.com>
2022-08-11 13:35:40 +0000

    soc: am64x: move to latest silicon revision 2.0 specdata
    Moving to PG2.0 version of SOC JSON - AM64_PG2_SoC_spec_220809_192501_rls.
    
    No change to code, only data version comment update.
    
    Signed-off-by: Vishal Mahaveer <vishalm@ti.com>

--- rm/drivers/irq/soc/am64x/irq_cfg.h
+++ rm/drivers/irq/soc/am64x/irq_cfg.h
@@ -1,7 +1,7 @@
 /*
  * Interrupt Management
  *
- * Data version: 220519_153621
+ * Data version: 220809_192501
  *
  * Copyright (C) 2019-2022, Texas Instruments Incorporated
  * All rights reserved.
