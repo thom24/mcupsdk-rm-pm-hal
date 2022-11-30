a6c0af5095c56181f762f7309fbf08f46c789f5c
Sebin Francis <sebin.francis@ti.com>
2022-11-30 12:02:43 +0000

    lpm: Add support for MCU ONLY lpm
    Most of the code is added for MCU ONLY low power mode. In the top level
    handler update the if checks to check for MCU ONLY mode also.
    
    Signed-off-by: Sebin Francis <sebin.francis@ti.com>

--- pm/drivers/lpm/lpm_handler.c
+++ pm/drivers/lpm/lpm_handler.c
@@ -251,7 +251,7 @@ s32 dm_prepare_sleep_handler(u32 *msg_recv)
 	u8 mode = req->mode;
 
 	/* Only DEEP_SLEEP mode supported at the moment */
-	if (mode == TISCI_MSG_VALUE_SLEEP_MODE_DEEP_SLEEP) {
+	if (mode == TISCI_MSG_VALUE_SLEEP_MODE_DEEP_SLEEP || mode == TISCI_MSG_VALUE_SLEEP_MODE_MCU_ONLY) {
 		/* Parse and store the mode info and ctx address in the prepare sleep message*/
 		lpm_populate_prepare_sleep_data(req);
 
@@ -283,7 +283,7 @@ s32 dm_enter_sleep_handler(u32 *msg_recv)
 	enter_sleep_status = 0;
 
 	/* Only DEEP_SLEEP mode supported at the moment */
-	if (mode != TISCI_MSG_VALUE_SLEEP_MODE_DEEP_SLEEP) {
+	if (mode != TISCI_MSG_VALUE_SLEEP_MODE_DEEP_SLEEP && mode != TISCI_MSG_VALUE_SLEEP_MODE_MCU_ONLY) {
 		ret = EINVAL;
 	}
 
