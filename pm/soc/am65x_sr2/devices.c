4da1933e08d9146d9d392bd545c2cf591083084d
Dillon Frank <d-frank@ti.com>
2023-01-31 22:30:46 +0000

    soc: am62x: am65x: Pull in latest autogen updates
    This brings the am62x and am65x_sr2 soc specific files
    up to date with autogen. Most changes are simply
    updating the copyright date to 2023
    
    Fixes: SYSFW-5967
    
    Signed-off-by: Dillon Frank <d-frank@ti.com>

--- pm/soc/am65x_sr2/devices.c
+++ pm/soc/am65x_sr2/devices.c
@@ -1,7 +1,7 @@
 /*
  * Data version: 000000_000000
  *
- * Copyright (C) 2017-2021 Texas Instruments Incorporated - http://www.ti.com/
+ * Copyright (C) 2017-2023 Texas Instruments Incorporated - http://www.ti.com/
  * ALL RIGHTS RESERVED
  */
 #include <types/array_size.h>
--- rm/drivers/irq/soc/am62x/irq_cfg.h
+++ rm/drivers/irq/soc/am62x/irq_cfg.h
@@ -3,7 +3,7 @@
  *
  * Data version: 221107_151433
  *
- * Copyright (C) 2021-2022, Texas Instruments Incorporated
+ * Copyright (C) 2021-2023, Texas Instruments Incorporated
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
