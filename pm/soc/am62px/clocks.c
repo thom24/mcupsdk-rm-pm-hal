901c73106c2b5ee348b6e2b1581be0d25e6000a6
Vishal Mahaveer <vishalm@ti.com>
2023-04-14 14:17:34 +0000

    pm: am62x/am62ax/am62px: Set NO_HW_REINIT flag for MPU and GTC PLLs
    Set clk_no_hw_reinit flags for MPU and GTC PLLs.
    
    MPU: MAIN PLL8, HSDIV0
    GTC: MAIN PLL0, HSDIV6
    
    This is to prevent these PLLs from being re-initialized by DM
    firmware and overwrite the configuration done by SPL.
    
    Fixes: SYSFW-5777
    
    Signed-off-by: Vishal Mahaveer <vishalm@ti.com>

--- pm/soc/am62px/clocks.c
+++ pm/soc/am62px/clocks.c
@@ -3993,7 +3993,7 @@ const struct clk_data soc_clock_data[274] = {
 		.drv		= &clk_drv_pll_16fft,
 		.freq_idx	= AM62PX_FREQ_VALUE_PLLFRACF2_SSMOD_16FFT_MAIN_0,
 		.data		= &clk_data_pllfracf2_ssmod_16fft_main_0.data_pll.data,
-		.flags		= 0,
+		.flags		= CLK_DATA_FLAG_NO_HW_REINIT,
 	},
 	[CLK_AM62PX_PLLFRACF2_SSMOD_16FFT_MAIN_0_FOUTPOSTDIV_CLK] =				{
 		.parent =									{
@@ -4123,7 +4123,7 @@ const struct clk_data soc_clock_data[274] = {
 		.drv		= &clk_drv_pll_16fft,
 		.freq_idx	= AM62PX_FREQ_VALUE_PLLFRACF2_SSMOD_16FFT_MAIN_8,
 		.data		= &clk_data_pllfracf2_ssmod_16fft_main_8.data_pll.data,
-		.flags		= 0,
+		.flags		= CLK_DATA_FLAG_NO_HW_REINIT,
 	},
 	[CLK_AM62PX_PLLFRACF2_SSMOD_16FFT_MCU_0_FOUTVCOP_CLK] =					{
 		.parent		=								{
@@ -4575,7 +4575,7 @@ const struct clk_data soc_clock_data[274] = {
 			1,
 		},
 		.drv	= &clk_drv_div_pll_16fft_hsdiv.drv,
-		.flags	= CLK_DATA_FLAG_MODIFY_PARENT_FREQ,
+		.flags	= CLK_DATA_FLAG_MODIFY_PARENT_FREQ | CLK_DATA_FLAG_NO_HW_REINIT,
 		.type	= CLK_TYPE_DIV,
 		.data	= &clk_data_hsdiv0_16fft_main_8_hsdiv0.data_div.data,
 	},
