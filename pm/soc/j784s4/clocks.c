176a226818405dfb858d40d3e65e1cd0784ee041
Chandru Dhavamani <a0497642@ti.com>
2023-05-28 13:52:09 +0000

    clock: soc: j721s2/j784s4: Change Freq of OSPI clk
    Change frequency of OSPI clk to 166MHz during
    DM initialization.
    
    Source of OSPI clk is mcu_pll2_hsdiv4.
    VCO frequency of mcu pll2 is 2GHz, change the
    HSDIV4 value of mcu pll2 to 12 to obtain 166MHz.
    
    Fixes: SYSFW-6315
    
    Signed-off-by: Chandru Dhavamani <a0497642@ti.com>

--- pm/soc/j784s4/clocks.c
+++ pm/soc/j784s4/clocks.c
@@ -6791,7 +6791,7 @@ static const struct clk_data_div_reg clk_data_hsdiv4_16fft_mcu_2_hsdiv3 = {
 static const struct clk_data_div_reg clk_data_hsdiv4_16fft_mcu_2_hsdiv4 = {
 	.data_div		= {
 		.n		= 128,
-		.default_div	= 6,
+		.default_div	= 12,
 	},
 	.reg			= 0x40D00000UL + (0x1000UL * 2UL) + 0x80UL + (0x4UL * 4UL),
 	.bit			= 0,
