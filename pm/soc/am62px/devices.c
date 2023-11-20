2e15454adc1842acdbd620f5aa29f95201167208
Vishal Mahaveer <vishalm@ti.com>
2023-11-20 16:56:44 +0000

    Revert "pm: am62/am62a/am62p: Remove the keep alive flags from USB LPSC"
    This reverts commit b88817d63e4c0cb8c5daa0fe8cccb64d4fd174d5.
    
    This change introduces regression with USB DeepSleep wakeup use case
    and also with runtime PM use case. The USB IP supports runtime low power
    states and is not dependent on SoC level LPSC to be turned off.
    
    Fixes: SYSFW-6832
    
    Signed-off-by: Vishal Mahaveer <vishalm@ti.com>

--- pm/soc/am62px/devices.c
+++ pm/soc/am62px/devices.c
@@ -1919,7 +1919,7 @@ static const struct lpsc_module_data am62px_sam67_main_psc_wrap_main_0_psc_0_mod
 			0,
 			0,
 		},
-		.flags			= LPSC_MODULE_EXISTS | LPSC_DEPENDS,
+		.flags			= LPSC_MODULE_EXISTS | LPSC_DEPENDS | LPSC_NO_CLOCK_GATING | LPSC_NO_MODULE_RESET,
 	},
 	[AM62PX_PSC_LPSC_LPSC_MAIN_USB2_ISO] =		{
 		.powerdomain		= AM62PX_PSC_PD_GP_CORE,
@@ -1931,7 +1931,7 @@ static const struct lpsc_module_data am62px_sam67_main_psc_wrap_main_0_psc_0_mod
 			0,
 			0,
 		},
-		.flags			= LPSC_MODULE_EXISTS | LPSC_DEPENDS,
+		.flags			= LPSC_MODULE_EXISTS | LPSC_DEPENDS | LPSC_NO_CLOCK_GATING | LPSC_NO_MODULE_RESET,
 	},
 	[AM62PX_PSC_LPSC_LPSC_MAIN_TEST] =		{
 		.powerdomain		= AM62PX_PSC_PD_GP_CORE,
